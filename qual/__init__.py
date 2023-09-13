96a15b2635492d7bfcea4f8843ef981d72f8ee4c
kaluluosi111 <kaluluosi@gmail.com>
2023-09-13 15:43:20 +0800

    提交样本env文件

--- qual/__init__.py
+++ qual/__init__.py
@@ -2,7 +2,7 @@ import qual.apps  # noqa: F401
 import qual.core.xyapi as xyapi
 from fastapi import FastAPI
 from uvicorn import run
-from qual.core.config import settings, Enviroment
+from qual.core.config import settings, Environment
 
 
 app = FastAPI(debug=settings.DEBUG)
@@ -10,5 +10,5 @@ xyapi.init(app)
 
 
 def serve():
-    reload = settings.ENVIROMENT == Enviroment.development
+    reload = settings.ENVIROMENT == Environment.development
     run("qual:app", reload=reload, host=settings.HOST, port=settings.PORT)
--- qual/core/config.py
+++ qual/core/config.py
@@ -2,13 +2,13 @@ import enum
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Enviroment(enum.StrEnum):
+class Environment(enum.StrEnum):
     development = "dev"
     production = "prod"
 
 
 class Settings(BaseSettings):
-    ENVIROMENT: Enviroment = Enviroment.development
+    ENVIRONMENT: Environment = Environment.development
     API_PATH: str = "/api/v1"
     DEBUG: bool = True
     # 密码hash用
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
     HOST: str = "127.0.0.1"
     PORT: int = 8000
 
-    DB_DSN: str = "sqlite:///db.sqlite"
+    DB_DSN: str = "sqlite:///.db.sqlite"
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
