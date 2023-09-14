47a30d42d34c6f369bf5a08946927eae85ac1054
kaluluosi111 <kaluluosi@gmail.com>
2023-09-14 18:56:42 +0800

    更新 init

--- backend/qual/__init__.py
+++ backend/qual/__init__.py
@@ -1,9 +1,11 @@
-import qual.apps  # noqa
+import qual.apps
+from qual.core import xyapi
 from fastapi import FastAPI
 from uvicorn import run
 from qual.core.config import settings, Environment
 
 app = FastAPI(debug=settings.DEBUG)
+xyapi.init(app, qual)
 
 
 def serve():
