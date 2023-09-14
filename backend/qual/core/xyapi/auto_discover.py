0e2133dbd0e93cb7a76d6dd2ff64b0659407e07f
kaluluosi111 <kaluluosi@gmail.com>
2023-09-14 14:32:41 +0800

    调整参数名和注释

--- backend/qual/core/xyapi/auto_discover.py
+++ backend/qual/core/xyapi/auto_discover.py
@@ -39,32 +39,33 @@ def get_module_dir(module: ModuleType | str):
 def discover(module: ModuleType | str, pattern: str = "*"):
     """扫描发现 `module` 下匹配 `pattern` 的模块文件，返回模块路径。
 
-    由于这个函数是用来发现python文件模块的，因此默认匹配 `.py` 文件。
-    `pattern` 参数只需要填写文件名的通配符规则即可。
+        由于这个函数是用来发现python文件模块的，因此默认匹配 `.py` 文件。
+        `pattern` 参数只需要填写文件名的通配符规则即可。
 
-    NOTE: 不会忽略 `_` `__` 开头的模块，完全依据 `pattern` 来匹配模块，没有暗箱操作。
+        NOTE: 不会忽略 `_` `__` 开头的模块，完全依据 `pattern` 来匹配模块，没有暗箱操作。
 
-    用例:
+        用例:
 
-    ```python
-    module_paths = discover(qual, "__") # <- 找出 `qual` 模块中所有下划线开头的模块
-    >> [qual.__init__,...]
-    ```
+        ```python
+        module_paths = discover(qual, "__") # <- 找出 `qual` 模块中所有下划线开头的模块
+        >> [qual.__init__,...]
+        ```
 
-    返回的模块路径一定是完整的包路径，比如如果
+        返回的模块路径一定是完整的包路径，比如如果
 
-    ```python
+        ```python
     module_paths = discover(qual.core, "__") # <- 找出 `qual` 模块中所有下划线开头的模块
-    >> [qual.core.__init__, ...] <- 返回的模块路径也是完整的 `qual.core.xxx`
-    而不会是 `core.xxx`
-    ```
+        >> [qual.core.__init__, ...] <- 返回的模块路径也是完整的 `qual.core.xxx`
+        而不会是 `core.xxx`
+        ```
 
-    Args:
-        module (ModuleType | str): 模块或模块路径（如：`qual.core`)
-        pattern (str): 通配符匹配规则，如 `model*`
+        Args:
+            module (ModuleType | str): 模块或模块路径（如：`qual.core`)
 
-    Yields:
-        _type_: _description_
+            pattern (str): 通配符匹配规则，如 `model*`
+
+        Yields:
+            _type_: _description_
     """
     if isinstance(module, ModuleType):
         if not module.__package__:
@@ -78,8 +79,9 @@ def discover(module: ModuleType | str, pattern: str = "*"):
 
     pattern = os.path.join("**", "*", file_pattern)  # <- 'package/**/*/<file_pattern>
 
-    # glob有个问题 `/**/*` 匹配只能匹配出子目录，无法匹配根目录文件
-    # FIXME: 不知道有没有更好的方法，有的话改掉
+    # FIXME: glob有个问题 `/**/*` 匹配只能匹配出子目录，无法匹配根目录文件。
+    # 现在之恶能够再做一个 `root_fiels` 的匹配把根目录文件匹出来。
+    # 不知道有没有更好的方法，有的话改掉。
     files = glob(pattern, root_dir=module_dir, recursive=True)
 
     # 因此在这里要多做一次根目录文件的匹配
@@ -93,15 +95,17 @@ def discover(module: ModuleType | str, pattern: str = "*"):
 
 def auto_discover(module: ModuleType | str, pattern: str = "*"):
     """自动发现并导入模块。
-    这个函数跟 `discover` 不同的是， `discover` 只发现和返回所有模块路径，这个函数则对所
+    这个函数跟 `discover` 不同的是， `discover` 只发现和返回所有模块路径，这个函数则对所p>
     有模块执行 `import_module`。
 
+
     Args:
-        module (ModuleType | str): _description_
-        pattern (str, optional): _description_. Defaults to "*".
+        module (ModuleType | str): 模块或者模块路径
+
+        pattern (str, optional): 模块文件名匹配规则，例如 `model*`. Defaults to "*".
 
     Returns:
-        _type_: _description_
+        dict[str,ModuleType]: 导入后的模块字典
     """
 
     module_paths = discover(module, pattern)
@@ -109,7 +113,14 @@ def auto_discover(module: ModuleType | str, pattern: str = "*"):
     modules: dict[str, ModuleType] = {}
     for module_path in module_paths:
         module = import_module(module_path)
-        logger.info(f"Auto discover module: {module_path}")
+        logger.debug(f"Auto discover module: {module_path}")
         modules[module_path] = module
 
     return modules
+
+
+def test():
+    from importlib.metadata import packages_distributions
+
+    dist = packages_distributions()
+    print(dist)
