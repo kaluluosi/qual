8e1acdca43e0c78a070f96e70cd36d74325c36af
kaluluosi111 <kaluluosi@gmail.com>
2023-09-21 17:16:45 +0800

    不要将sso.api添加到auth子路由，会影响sso bearer的tokenurl获取。

--- backend/qual/apps/auth/__app__.py
+++ backend/qual/apps/auth/__app__.py
@@ -7,6 +7,5 @@ from .router import api
 @installer(__name__)
 def install(app: FastAPI):
     # 安装sso登录接口
-    api.include_router(sso.api)
-
+    app.include_router(sso.api)
     app.include_router(api)
