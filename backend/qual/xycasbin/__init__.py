0f2bd16b7281e8198db0bd712f5da16045c8bb90
kaluluosi111 <kaluluosi@gmail.com>
2023-09-28 11:41:41 +0000

    提交 casbin 适配新思路

--- backend/qual/xycasbin/__init__.py
+++
@@ -1,7 +0,0 @@
-import casbin
-import pkg_resources
-
-model_conf = pkg_resources.resource_filename(__package__, "model.conf")  # 模型配置
-policy = pkg_resources.resource_filename(__package__, "policy")  # 规则/适配器
-
-e = casbin.Enforcer(model=model_conf, adapter=policy)
